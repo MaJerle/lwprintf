3cd76842ff51c289936d0b80cfcffaf685c933dc
Tilen Majerle <tilen.majerle@gmail.com>
2020-03-21 01:51:43 +0100

    Add main test code

--- dev/VisualStudio/main.c
+++ dev/VisualStudio/main.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include "lwprintf/lwprintf.h"
-#include "string.h"
-#include "stdint.h"
+#include <string.h>
+#include <stdint.h>
+
+int
+lwprintf_output(int ch, struct lwprintf* lw) {
+    printf("%c", (char)ch);
+    return ch;
+}
+
+lwprintf_t lw;
 
 int
 main(void) {
 
+    lwprintf_init(&lw, lwprintf_output);
+    lwprintf_printf(&lw, "Number: %d\r\n", 13);
     return 0;
 }
