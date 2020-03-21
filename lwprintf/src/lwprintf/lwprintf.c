480b22654f296828fb3fbbaa1b47e003623b0292
Tilen Majerle <tilen.majerle@gmail.com>
2020-03-21 15:13:03 +0100

    Put all working data to single structure and add snprintf function. Implement few options only for now

--- lwprintf/src/lwprintf/lwprintf.c
+++ lwprintf/src/lwprintf/lwprintf.c
@@ -42,47 +42,78 @@
 #define CHARTONUM(x)                    ((x) - '0')
 
 /**
+ * \brief           Forward declaration
+ */
+struct lwprintf_int;
+
+/**
  * \brief           Private output function declaration
  */
-typedef int (*prv_output_fn)(lwprintf_t* lw, char* const buff, const char c, const size_t id, size_t buff_size);
+typedef int (*prv_output_fn)(struct lwprintf_int* lwi, const char c);
 
-/**
- * \brief           List of flags part for format
- */
-typedef union {
+typedef struct lwprintf_int {
+    lwprintf_t* lw;                             /*!< Instance */
+    const char* fmt;                            /*!< Format strin */
+    char* const buff;                           /*!< Pointer to buffer when not using print option */
+    const size_t buff_size;                     /*!< Buffer size of input buffer (when used) */
+    size_t n;                                   /*!< Full length of formatted text */
+    prv_output_fn out_fn;                       /*!< Output internal function */
+
+    /* This must all be reset every time new % is detected */
     struct {
-        uint8_t left_align : 1;                 /*!< Minus for left alignment */
-        uint8_t plus : 1;                       /*!< Prepend + for positive numbers on the output */
-        uint8_t space : 1;                      /*!< Prepend spaces. Not used with plus modifier */
-        uint8_t zero : 1;                       /*!< Zero pad flag detection, add zeros if number length is less than width modifier */
-        uint8_t thousands:1;                    /*!< Thousands has grouping applied */
-        uint8_t hash : 1;
-        uint8_t precision : 1;                  /*!< Precision flag has been used */
+        struct {
+            uint8_t left_align : 1;             /*!< Minus for left alignment */
+            uint8_t plus : 1;                   /*!< Prepend + for positive numbers on the output */
+            uint8_t space : 1;                  /*!< Prepend spaces. Not used with plus modifier */
+            uint8_t zero : 1;                   /*!< Zero pad flag detection, add zeros if number length is less than width modifier */
+            uint8_t thousands : 1;              /*!< Thousands has grouping applied */
+            uint8_t hash : 1;
+            uint8_t precision : 1;              /*!< Precision flag has been used */
 
-        /* Length modified flags */
-        uint8_t l : 1;                          /*!< Flag indicating long */
-        uint8_t ll : 1;                         /*!< Flag indicatin long-long number */
-    } f;
-    uint16_t flags;
-} prv_flags_t;
-
-typedef struct {
-    prv_flags_t flags;
-    int precision;
-    int width;
-} prv_parser_t;
+            /* Length modified flags */
+            uint8_t longlong : 2;               /*!< Flag indicatin long-long number */
+            uint8_t uc : 1;                     /*!< Uppercase flag */
+        } flags;
+        int precision;                          /*!< Selected precision */
+        int width;                              /*!< Text width indicator */
+        uint8_t base;                           /*!< Base for number format output */
+    } m;
+} lwprintf_int_t;
 
 /**
- * \brief           Output function for printf
- * \param[in]       ...
+ * \brief           Output function to print data
+ * \param[in]       lwi: Internal working structure
+ * \param[in]       c: Character to print
+ * \return          `1` on success, `0` otherwise
  */
 static int
-prv_out_fn_print(lwprintf_t* lw, char* const buff, const char c, const size_t id, size_t buff_size) {
-    lw->out(c, lw);                             /*!< Send character to output */
-
+prv_out_fn_print(lwprintf_int_t* lwi, const char c) {
+    lwi->lw->out(c, lwi->lw);                   /*!< Send character to output */
+    ++lwi->n;
     return 1;
 }
 
+/**
+ * \brief           Output function to generate buffer data
+ * \param[in]       lwi: Internal working structure
+ * \param[in]       c: Character to write
+ * \return          `1` on success, `0` otherwise
+ */
+static int
+prv_out_fn_write_buff(lwprintf_int_t* lwi, const char c) {
+    if (lwi->n < (lwi->buff_size - 1)) {
+        lwi->buff[lwi->n++] = c;
+        lwi->buff[lwi->n] = '\0';
+        return 1;
+    }
+    return 0;
+}
+
+/**
+ * \brief           Parse number from input string
+ * \param[in,out]   format: Input text to process
+ * \return          Parsed number
+ */
 static int
 prv_parse_num(const char** format) {
     const char* fmt = *format;
@@ -95,39 +126,74 @@ prv_parse_num(const char** format) {
     return n;
 }
 
+static int
+unsigned_int_to_str(lwprintf_int_t* p, unsigned int num) {
+    char buff[12];
+    unsigned int d;
+    uint8_t digits_cnt;
+    char c;
+
+    /* Format string */
+    for (digits_cnt = 0; num > 0; num /= p->m.base, ++digits_cnt) {
+        d = num % p->m.base;
+        c = d + (d > 10 ? ((p->m.flags.uc ? 'A' : 'a') - 10) : '0');
+        buff[digits_cnt] = c;
+    }
+    
+    /* Rotate string */
+    for (uint8_t i = 0; i < digits_cnt / 2; ++i) {
+        char t = buff[i];
+        buff[i] = buff[digits_cnt - i - 1];
+        buff[digits_cnt - i - 1] = t;
+    }
+
+    /* Output string */
+    for (uint8_t i = 0; i < digits_cnt; ++i) {
+        p->out_fn(p, buff[i]);
+    }
+    return digits_cnt;
+}
+
+static int
+unsigned_long_int_to_str(lwprintf_int_t* p, unsigned long int num) {
+
+}
+
+static int
+unsigned_longlong_int_to_str(lwprintf_int_t* p, unsigned long long int num) {
+
+}
+
 static uint8_t
-prv_format(lwprintf_t* const lw, const prv_output_fn out_fn,
-            const char* fmt, char* buff,
-            const size_t buff_size, va_list vl) {
-    prv_parser_t p;
-    size_t idx = 0;
+prv_format(lwprintf_int_t* p, va_list vl) {
     uint8_t detected = 0;
+    const char* fmt = p->fmt;
 
     while (fmt != NULL && *fmt != '\0') {
         /* Parse format */
         /* %[flags][width][.precision][length]type */
         /* Go to https://en.wikipedia.org/wiki/Printf_format_string for more info */
+        memset(&p->m, 0x00, sizeof(p->m));      /* Reset structure */
 
         /* Detect beginning */
         if (*fmt != '%') {
-            out_fn(lw, buff, *fmt, idx++, buff_size);   /* Output character */
+            p->out_fn(p, *fmt);                 /* Output character */
             ++fmt;
             continue;
         }
-        fmt++;
+        ++fmt;
 
         /* Check [flags] */
-        /* If can have multiple flags in any order */
-        p.flags.flags = 0;
+        /* It can have multiple flags in any order */
         do {
             detected = 1;
             switch (*fmt) {
-                case '-':   p.flags.f.left_align = 1;   break;
-                case '+':   p.flags.f.plus = 1;         break;
-                case ' ':   p.flags.f.space = 1;        break;
-                case '0':   p.flags.f.zero = 1;         break;
-                case '\'':  p.flags.f.thousands = 1;    break;
-                case '#':   p.flags.f.hash = 1;         break;
+                case '-':   p->m.flags.left_align = 1;  break;
+                case '+':   p->m.flags.plus = 1;        break;
+                case ' ':   p->m.flags.space = 1;       break;
+                case '0':   p->m.flags.zero = 1;        break;
+                case '\'':  p->m.flags.thousands = 1;   break;
+                case '#':   p->m.flags.hash = 1;        break;
                 default:    detected = 0;               break;
             }
             if (detected) {
@@ -136,21 +202,21 @@ prv_format(lwprintf_t* const lw, const prv_output_fn out_fn,
         } while (detected);
 
         /* Check [width] */
-        p.width = 0;
+        p->m.width = 0;
         if (CHARISNUM(*fmt)) {
-            p.width = prv_parse_num(&fmt);
+            p->m.width = prv_parse_num(&fmt);
         }
 
         /* Check [.precision] */
-        p.precision = 0;
+        p->m.precision = 0;
         if (*fmt == '.') {
-            p.flags.f.precision = 1;
+            p->m.flags.precision = 1;
             ++fmt;
             if (*fmt == '*') {
                 const int pr = (int)va_arg(vl, int);
-                p.precision = pr > 0 ? pr : 0;
+                p->m.precision = pr > 0 ? pr : 0;
             } else if (CHARISNUM(*fmt)) {
-                p.precision = prv_parse_num(&fmt);
+                p->m.precision = prv_parse_num(&fmt);
             }
         }
 
@@ -158,10 +224,11 @@ prv_format(lwprintf_t* const lw, const prv_output_fn out_fn,
         detected = 1;
         switch (*fmt) {
             case 'l':
-                p.flags.l = 1;
+                p->m.flags.longlong = 1;
                 ++fmt;
                 if (*fmt == 'l') {
-                    p.flags.ll = 1;
+                    p->m.flags.longlong = 2;
+                    ++fmt;
                 }
                 break;
             case 'L': break;
@@ -173,34 +240,98 @@ prv_format(lwprintf_t* const lw, const prv_output_fn out_fn,
 
         /* Check type */
         switch (*fmt) {
+            case 'c':
+                p->out_fn(p, va_arg(vl, char));
+                break;
             case 'd':
-            case 'D':
-                out_fn(lw, buff, 'd', idx++, buff_size);
+            case 'i': {
+                p->m.base = 10;
+                int num = va_arg(vl, int);
+                p->out_fn(p, 'd');
+                break;
+            }
+            case 'b':
+            case 'B':
+                p->m.base = 2;
+                unsigned_int_to_str(p, va_arg(vl, unsigned int));
+                break;
+            case 'o':
+                p->m.base = 8;
+                unsigned_int_to_str(p, va_arg(vl, unsigned int));
+                break;
+            case 'u':
+                p->m.base = 10;
+                unsigned_int_to_str(p, va_arg(vl, unsigned int));
+                break;
+            case 'x':
+            case 'X':
+                p->m.base = 16;
+                p->m.flags.uc = *fmt == 'X';    /* Select if uppercase text shall be printed */
+                unsigned_int_to_str(p, va_arg(vl, unsigned int));
+                break;
+            case '%':
+                p->out_fn(p, '%');
                 break;
             default:
-                out_fn(lw, buff, *fmt, idx++, buff_size);
+                p->out_fn(p, *fmt);
         }
         ++fmt;
     }
-
-    return idx;
+    return 1;
 }
 
+/**
+ * \brief           Initialize LwPRINTF instance
+ * \param[in,out]   lw: LwPRINTF working instance
+ * \param[in]       out_fn: Output function used for print operation
+ * \return          `1` on success, `0` otherwise
+ */
 uint8_t
 lwprintf_init(lwprintf_t* lw, lwprintf_output_fn out_fn) {
     lw->out = out_fn;
     return 1;
 }
 
+/**
+ * \brief           Print data to the output directly
+ * \param[in,out]   lw: LwPRINTF working instance.
+ *                      Set to `NULL` to use default one
+ * \param[in]       fmt: Format string
+ * \param[in]       ... Optional arguments for format
+ * \return          `1` on success, `0` otherwise
+ */
 int
 lwprintf_printf(lwprintf_t* const lw, const char* fmt, ...) {
     va_list va;
-    char buff[1];
-    int n;
+    lwprintf_int_t format = {
+        .lw = lw,
+        .out_fn = prv_out_fn_print,
+        .fmt = fmt,
+        .buff = NULL,
+        .buff_size = 0
+    };
 
     va_start(va, fmt);
-    n = prv_format(lw, prv_out_fn_print, fmt, buff, sizeof(buff), va);
+    prv_format(&format, va);
     va_end(va);
 
-    return n;
+    return format.n;
+}
+
+int
+lwprintf_snprintf(lwprintf_t* const lw, char* buff, size_t buff_size, const char* fmt, ...) {
+    va_list va;
+    lwprintf_int_t format = {
+        .lw = lw,
+        .out_fn = prv_out_fn_write_buff,
+        .fmt = fmt,
+        .buff = buff,
+        .buff_size = buff_size
+    };
+
+    va_start(va, fmt);
+    prv_format(&format, va);
+    va_end(va);
+
+    return format.n;
 }
