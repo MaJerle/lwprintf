8d501de84943d1bdb8da53d2c3a8dc9fa9321fac
Tilen Majerle <tilen.majerle@gmail.com>
2020-09-27 09:53:07 +0200

    Add macro with int cast

--- examples/additional_format_specifiers.c
+++ examples/additional_format_specifiers.c
@@ -10,7 +10,7 @@ additional_format_specifiers(void) {
     unsigned char my_array[] = { 0x01, 0x02, 0xA4, 0xB5, 0xC6 };
 
     /* Binary output */
-    
+
     /* Prints number 8 in binary format, so "1000" */
     lwprintf_printf("%b\r\n", 8U);
     /* Prints number 16 in binary format with 10 places, so "     10000" */
@@ -25,11 +25,11 @@ additional_format_specifiers(void) {
     /* Fixed length with lowercase hex numbers, outputs "0102a4b5c6" */
     lwprintf_printf("%5k\r\n", my_array);
     /* Variable length with uppercase letters, outputs "0102A4B5C6" */
-    lwprintf_printf("%*K\r\n", sizeof(my_array) / sizeof(my_array[0]), my_array);
+    lwprintf_printf("%*K\r\n", (int)LWPRINTF_ARRAYSIZE(my_array), my_array);
     /* Variable length with lowercase letters, outputs "0102a4b5c6" */
-    lwprintf_printf("%*k\r\n", sizeof(my_array) / sizeof(my_array[0]), my_array);
+    lwprintf_printf("%*k\r\n", (int)LWPRINTF_ARRAYSIZE(my_array), my_array);
     /* Variable length with uppercase letters and spaces, outputs "01 02 A4 B5 C6" */
-    lwprintf_printf("% *K\r\n", sizeof(my_array) / sizeof(my_array[0]), my_array);
+    lwprintf_printf("% *K\r\n", (int)LWPRINTF_ARRAYSIZE(my_array), my_array);
     /* Variable length with uppercase letters and spaces, outputs "01 02 a4 b5 c6" */
-    lwprintf_printf("% *k\r\n", sizeof(my_array) / sizeof(my_array[0]), my_array);
+    lwprintf_printf("% *k\r\n", (int)LWPRINTF_ARRAYSIZE(my_array), my_array);
 }
