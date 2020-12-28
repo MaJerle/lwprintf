e2d9125442a53b5060ab1d80f7cfae09374f290e
Tilen Majerle <tilen.majerle@gmail.com>
2020-12-28 16:15:08 +0100

    Add version v1.0.1

--- lwprintf/src/lwprintf/lwprintf.c
+++ lwprintf/src/lwprintf/lwprintf.c
@@ -29,7 +29,7 @@
  * This file is part of LwPRINTF - Lightweight stdio manager library.
  *
  * Author:          Tilen MAJERLE <tilen@majerle.eu>
- * Version:         v1.0.0
+ * Version:         v1.0.1
  */
 #include <limits.h>
 #include <float.h>
