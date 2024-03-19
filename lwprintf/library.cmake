3c3b74c36332486e16af61fe8b031228f4574302
Tilen Majerle <tilen@majerle.eu>
2024-03-19 21:34:25 +0100

    Update library CMake system, to support automatic creation of options file, if user doesn't provide one

--- lwprintf/library.cmake
+++ lwprintf/library.cmake
@@ -1,14 +1,19 @@
 # 
+# LIB_PREFIX: LWPRINTF
+#
 # This file provides set of variables for end user
 # and also generates one (or more) libraries, that can be added to the project using target_link_libraries(...)
 #
 # Before this file is included to the root CMakeLists file (using include() function), user can set some variables:
 #
 # LWPRINTF_SYS_PORT: If defined, it will include port source file from the library.
-# LWPRINTF_OPTS_DIR: If defined, it should set the folder path where options file shall be generated.
+# LWPRINTF_OPTS_FILE: If defined, it is the path to the user options file. If not defined, one will be generated for you automatically
 # LWPRINTF_COMPILE_OPTIONS: If defined, it provide compiler options for generated library.
 # LWPRINTF_COMPILE_DEFINITIONS: If defined, it provides "-D" definitions to the library build
 #
+
+# Custom include directory
+set(LWPRINTF_CUSTOM_INC_DIR ${CMAKE_CURRENT_BINARY_DIR}/lib_inc)
 
 # Library core sources
 set(lwprintf_core_SRCS 
@@ -26,6 +31,7 @@ endif()
 # Setup include directories
 set(lwprintf_include_DIRS
     ${CMAKE_CURRENT_LIST_DIR}/src/include
+    ${LWPRINTF_CUSTOM_INC_DIR}
 )
 
 # Register library to the system
@@ -35,7 +41,11 @@ target_include_directories(lwprintf INTERFACE ${lwprintf_include_DIRS})
 target_compile_options(lwprintf PRIVATE ${LWPRINTF_COMPILE_OPTIONS})
 target_compile_definitions(lwprintf PRIVATE ${LWPRINTF_COMPILE_DEFINITIONS})
 
-# Create config file
-if(DEFINED LWPRINTF_OPTS_DIR AND NOT EXISTS ${LWPRINTF_OPTS_DIR}/lwprintf_opts.h)
-    configure_file(${CMAKE_CURRENT_LIST_DIR}/src/include/lwprintf/lwprintf_opts_template.h ${LWPRINTF_OPTS_DIR}/lwprintf_opts.h COPYONLY)
+# Create config file if user didn't provide one info himself
+if(NOT LWPRINTF_OPTS_FILE)
+    message(STATUS "Using default lwprintf_opts.h file")
+    set(LWPRINTF_OPTS_FILE ${CMAKE_CURRENT_LIST_DIR}/src/include/lwprintf/lwprintf_opts_template.h)
+else()
+    message(STATUS "Using custom lwprintf_opts.h file from ${LWPRINTF_OPTS_FILE}")
 endif()
+configure_file(${LWPRINTF_OPTS_FILE} ${LWPRINTF_CUSTOM_INC_DIR}/lwprintf_opts.h COPYONLY)
